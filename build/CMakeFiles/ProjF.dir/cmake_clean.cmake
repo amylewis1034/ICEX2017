b6dce97290f379acd2e08447e0c7cfd5dfecaa6f
Amy Lewis <amylewis1034@gmail.com>
2017-06-07 12:56:46 -0700

    Changed background color - not permanent

---
+++ build/CMakeFiles/ProjF.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjF.dir/src/Bubbles.cpp.o"
+  "CMakeFiles/ProjF.dir/src/GLSL.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Image.cpp.o"
+  "CMakeFiles/ProjF.dir/src/ImageProcessor.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Link.cpp.o"
+  "CMakeFiles/ProjF.dir/src/MatrixStack.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Node.cpp.o"
+  "CMakeFiles/ProjF.dir/src/PRMAlg.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Program.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Shape.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Texture.cpp.o"
+  "CMakeFiles/ProjF.dir/src/Utilities.cpp.o"
+  "CMakeFiles/ProjF.dir/src/main.cpp.o"
+  "ProjF.pdb"
+  "ProjF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
